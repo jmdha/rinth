e4acc6f37bf2ee519ca5b284e4532bdedfb180fe
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2024-06-10 22:11:58 +0200

    misc: improve test runner output

--- tests/runner.c
+++ tests/runner.c
@@ -1,9 +1,20 @@
 #include <criterion/criterion.h>
 #include <stdio.h>
 
+#define RESET "\033[0m"
+#define RED "\033[31m"   /* Red */
+#define GREEN "\033[32m" /* Green */
+
 ReportHook(POST_TEST)(struct criterion_test_stats *stats) {
-    printf(
-        "[%12s.%-12s] finished in %f s\n", stats->test->category, stats->test->name,
-        stats->elapsed_time
-    );
+    if (stats->failed_asserts == 0) {
+        printf(
+            "[ %12s.%-12s ] [" GREEN "   OK   " RESET "] [ %f s ]\n", stats->test->category,
+            stats->test->name, stats->elapsed_time
+        );
+    } else {
+        printf(
+            "[ %12s.%-12s ] [" RED " FAILED " RESET "] [ %f s ]\n", stats->test->category,
+            stats->test->name, stats->elapsed_time
+        );
+    }
 }
