7dc671872958e6b37aa59abc7340fbe3a115226a
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2024-06-10 22:28:36 +0200

    feat: readme

---
+++ readme.md
@@ -0,0 +1,15 @@
+
+## Building
+To build the executable, simply run the following:
+```
+make build
+```
+The resulting executable will then be found in ./build/
+
+## Tests
+To run tests, simply run the following:
+```
+make test
+```
+> [!IMPORTANT]
+> The tests require [Criterion](https://github.com/Snaipe/Criterion). As such, it is required to be installed on the system. This is not automatic.
--- tests/runner.c
+++ tests/runner.c
@@ -6,14 +6,14 @@
 #define GREEN "\033[32m" /* Green */
 
 ReportHook(POST_TEST)(struct criterion_test_stats *stats) {
-    if (stats->failed_asserts == 0) {
+    if (stats->failed_asserts == 0 && !stats->crashed) {
         printf(
-            "[ %12s.%-12s ] [" GREEN "   OK   " RESET "] [ %f s ]\n", stats->test->category,
+            "[ %12s.%-12s ] [ %f s ] [" GREEN "   OK   " RESET "]\n", stats->test->category,
             stats->test->name, stats->elapsed_time
         );
     } else {
         printf(
-            "[ %12s.%-12s ] [" RED " FAILED " RESET "] [ %f s ]\n", stats->test->category,
+            "[ %12s.%-12s ] [ %f s ] [" RED " FAILED " RESET "]\n", stats->test->category,
             stats->test->name, stats->elapsed_time
         );
     }
