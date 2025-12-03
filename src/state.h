c519e2798026a6ebdda7c443d0390cbfe8d30dc1
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2025-12-03 18:32:04 +0100

    refactor: state

--- src/state.h
+++ src/state.h
@@ -7,21 +7,103 @@
 typedef struct state state; // A set of facts
 typedef struct state_iter state_iter; // An iterator of facts
 
-state* state_new();                                              // Create an empty state
-state* state_clone(const state*);                                // Clone a state
-bool   state_contains(const struct state*, u16, uint, u16*);     // Does state contain fact
-bool   state_equal   (const struct state*, const struct state*); // Are states equal
-bool   state_covers  (const struct state*, const struct state*); // Is the latter state a subset
-uint   state_overlap (const struct state*, const struct state*); // How many facts from former in latter
-bool   state_empty   (const struct state*);                      // Is state empty
-uint   state_count   (const struct state*);                      // How many facts in state
-uint   state_size    (const struct state*);                      // Size of state in bytes
-u64    state_hash    (const struct state*);                      // Generate a hash of state
-void   state_free  (struct state*);                              // Frees state allocation
-void   state_clear (struct state*);                              // Removes all facts from state
-void   state_insert(struct state*, u16, uint, u16*);             // Inserts fact into state
-void   state_remove(struct state*, u16, uint, u16*);             // Removes fact from state
+// Create an empty state
+state* state_new(
+	void
+);
 
-state_iter* state_iter_new(const struct state*);
-void        state_iter_free(struct state_iter*);
-bool        state_iter_step(struct state_iter*, u16*, uint*, u16*);
+// Clone a state
+state* state_clone(
+	const state* s
+);
+
+// Does state contain fact
+bool state_contains(
+	const struct state* s,
+	u16 predicate,
+	uint len,
+	u16* args
+);
+
+// Are states equal
+bool state_equal(
+	const struct state* a,
+	const struct state* b
+);
+
+// Is the latter state a subset
+bool state_covers(
+	const struct state* a,
+	const struct state* b
+);
+
+// How many facts from former in latter
+uint state_overlap(
+	const struct state* a,
+	const struct state* b
+);
+
+// Is state empty
+bool state_empty(
+	const struct state* s
+);
+
+// How many facts in state
+uint state_count(
+	const struct state* s
+);
+
+// Size of state in bytes
+uint state_size(
+	const struct state* s
+);
+
+// Generate a hash of state
+u64 state_hash(
+	const struct state* s
+);
+
+// Frees state allocation
+void state_free(
+	struct state* s
+);
+
+// Removes all facts from state
+void state_clear(
+	struct state* s
+);
+
+// Inserts facts into state
+void state_insert(
+	struct state* s,
+	u16 predicate,
+	uint len,
+	u16* args
+);
+
+// Removes fact from state
+void state_remove(
+	struct state* s,
+	u16 predicate,
+	uint len,
+	u16* args
+);
+
+// Create new state iter
+state_iter* state_iter_new(
+	const struct state* s
+);
+
+// Free memory of state iter
+void state_iter_free(
+	struct state_iter* si
+);
+
+// Do a single step of state iter
+// Returns false on no more steps
+bool state_iter_step(
+	struct state_iter* si,
+	u16* predicate,
+	uint* len,
+	u16* args
+);
