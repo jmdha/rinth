2bf36e509f71f0af0d0e533811c73a7ec8824cbc
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2025-12-31 08:22:38 +0100

    perf: cache element for pop

--- src/states.c
+++ src/states.c
@@ -3,7 +3,10 @@
 
 #include "states.h"
 
+#define GROWTH_FACTOR 4
+
 struct states {
+	size_t  ele;
 	size_t  len;
 	size_t  cap;
 	state** map;
@@ -12,6 +15,7 @@ struct states {
 states* states_new(void) {
 	states* S = malloc(sizeof(states));
 
+	S->ele = SIZE_MAX;
 	S->len = 0;
 	S->cap = 8;
 	S->map = calloc(S->cap, sizeof(state*));
@@ -37,20 +41,22 @@ bool states_contains(const states* S, const state* s) {
 	return false;
 }
 
-void states_insert(state** map, size_t cap, state* s) {
+size_t states_insert(state** map, size_t cap, state* s) {
 	const uint64_t hash = state_hash(s);
 
 	for (size_t o = 0; o < cap; o++) {
 		const uint64_t i = (hash + o) % cap;
 		if (!map[i]) {
 			map[i] = s;
-			return;
+			return i;
 		}
 	}
+
+	return SIZE_MAX;
 }
 
 void states_grow(states* S) {
-	const size_t cap = 2 * S->cap;
+	const size_t cap = GROWTH_FACTOR * S->cap;
 	state**      map = calloc(cap, sizeof(state*));
 	for (size_t i = 0; i < S->cap; i++)
 		if (S->map[i])
@@ -61,10 +67,11 @@ void states_grow(states* S) {
 }
 
 void states_add(states* S, state* s) {
-	if (2 * S->len > S->cap)
+	if (GROWTH_FACTOR * S->len > S->cap)
 		states_grow(S);
-	states_insert(S->map, S->cap, s);
+	size_t i = states_insert(S->map, S->cap, s);
 	S->len++;
+	S->ele = i;
 }
 
 void states_remove(states* S, state* s) {
@@ -75,16 +82,28 @@ void states_remove(states* S, state* s) {
 		if (S->map[i] && state_equal(S->map[i], s)) {
 			S->map[i] = NULL;
 			S->len--;
+			if (i == S->ele)
+				S->ele = SIZE_MAX;
 		}
 	}
 }
 
 state* states_pop(states* S) {
+	if (S->ele != SIZE_MAX) {
+		state *s = S->map[S->ele];
+		S->map[S->ele] = NULL;
+		S->len--;
+		S->ele = SIZE_MAX;
+		return s;
+	}
+
 	for (size_t i = 0; i < S->cap; i++)
 		if (S->map[i]) {
 			state* s = S->map[i];
 			S->len--;
 			S->map[i] = NULL;
+			if (i == S->ele)
+				S->ele = SIZE_MAX;
 			return s;
 		}
 
