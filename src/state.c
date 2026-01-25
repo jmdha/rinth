e4225491ae894477790534d4138c846adbfe948c
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2026-01-25 15:53:20 +0100

    misc: invert facts

--- src/state.c
+++ src/state.c
@@ -15,9 +15,9 @@ struct state {
 };
 
 static uint64_t fact_new(size_t predicate, size_t len, const size_t* args) {
-	uint64_t fact = (uint64_t)predicate << 48;
+	uint64_t fact = ((uint64_t)predicate + 1) << 48;
         for (size_t i = 0; i < len; i++)
-                fact |= (uint64_t)args[i] << 16 * (uint64_t)i;
+                fact |= ((uint64_t)args[i] + 1) << 16 * (uint64_t)i;
         return fact;
 }
 
@@ -29,9 +29,7 @@ state* state_new(void) {
         state* s = malloc(sizeof(state));
 	s->len   = 0;
 	s->cap   = 8;
-	s->buf   = malloc(s->cap * sizeof(uint64_t));
-	for (size_t i = 0; i < s->cap; i++)
-		s->buf[i] = SIZE_MAX;
+	s->buf   = calloc(s->cap, sizeof(uint64_t));
         return s;
 }
 
@@ -58,10 +56,10 @@ bool state_equal(const struct state* a, const struct state* b) {
 	if (a->cap == b->cap)
 		return memcmp(a->buf, b->buf, a->cap * sizeof(uint64_t)) == 0;
 	for (size_t i = 0; i < a->cap; i++)
-		if (a->buf[i] != SIZE_MAX && !state_contains_(b, a->buf[i]))
+		if (a->buf[i] && !state_contains_(b, a->buf[i]))
 			return false;
 	for (size_t i = 0; i < b->cap; i++)
-		if (b->buf[i] != SIZE_MAX && !state_contains_(a, b->buf[i]))
+		if (b->buf[i] && !state_contains_(a, b->buf[i]))
 			return false;
 	return true;
 }
@@ -70,7 +68,7 @@ bool state_covers(const struct state* a, const struct state* b) {
 	if (state_count(a) < state_count(b))
 		return false;
 	for (size_t i = 0; i < b->cap; i++) {
-		if (b->buf[i] == SIZE_MAX)
+		if (!b->buf[i])
 			continue;
 		if (!state_contains_(a, b->buf[i]))
 			return false;
@@ -81,7 +79,7 @@ bool state_covers(const struct state* a, const struct state* b) {
 size_t state_overlap(const struct state* a, const struct state* b) {
 	size_t count = 0;
 	for (size_t i = 0; i < a->cap; i++) {
-		if (a->buf[i] == SIZE_MAX)
+		if (!a->buf[i])
 			continue;
 		if (state_contains_(b, a->buf[i]))
 			count++;
@@ -104,7 +102,7 @@ size_t state_size(const struct state* s) {
 uint64_t state_hash(const struct state* s) {
 	uint64_t hash = 2166136261;
 	for (size_t i = 0; i < s->cap; i++) {
-		if (s->buf[i] == SIZE_MAX)
+		if (!s->buf[i])
 			continue;
 		hash ^= s->buf[i];
 		hash *= 1099511628211;
@@ -121,14 +119,12 @@ void state_clear(struct state* s) {
 	free(s->buf);
 	s->len = 0;
 	s->cap = 8;
-	s->buf = malloc(s->cap * sizeof(uint64_t));
-	for (size_t i = 0; i < s->cap; i++)
-		s->buf[i] = SIZE_MAX;
+	s->buf = calloc(s->cap, sizeof(uint64_t));
 }
 
 static bool insert_fact(uint64_t* buf, size_t cap, uint64_t fact) {
 	uint64_t index = fact_index(cap, fact);
-	if (buf[index] != SIZE_MAX)
+	if (buf[index])
 		return false;
 	buf[index] = fact;
 	return true;
@@ -137,7 +133,7 @@ static bool insert_fact(uint64_t* buf, size_t cap, uint64_t fact) {
 static bool copy_buf(uint64_t* buf, size_t cap, size_t old_cap, const uint64_t* old_buf) {
 	for (size_t i = 0; i < old_cap; i++) {
 		uint64_t fact = old_buf[i];
-		if (fact == SIZE_MAX)
+		if (!fact)
 			continue;
 		if (!insert_fact(buf, cap, fact))
 			return false;
@@ -151,7 +147,7 @@ void state_insert(struct state* s, size_t predicate, size_t len, const size_t* a
 	if (s->buf[index] == fact)
 		return; // Already in state
 	s->len++;
-	if (s->buf[index] == SIZE_MAX) {
+	if (!s->buf[index]) {
 		s->buf[index] = fact;
 		return; // Inserted without rebuild required
 	}
@@ -160,8 +156,7 @@ void state_insert(struct state* s, size_t predicate, size_t len, const size_t* a
 	do {
 		cap++;
 		buf = realloc(buf, cap * sizeof(uint64_t));
-		for (size_t i = 0; i < cap; i++)
-			buf[i] = SIZE_MAX;
+		memset(buf, 0, cap * sizeof(uint64_t));
 		buf[fact_index(cap, fact)] = fact;
 	} while (!copy_buf(buf, cap, s->cap, s->buf));
 	free(s->buf);
@@ -174,7 +169,7 @@ void state_remove(struct state* s, size_t predicate, size_t len, const size_t* a
 	size_t   index = fact_index(s->cap, fact);
 	if (s->buf[index] != fact)
 		return; // Not in state
-	s->buf[index] = SIZE_MAX;
+	s->buf[index] = 0;
 	s->len--;
 }
 
@@ -197,15 +192,15 @@ void state_iter_free(struct state_iter* si) {
 bool state_iter_step(struct state_iter* si, size_t* pred, size_t* len, size_t* args) {
 	while (si->index < si->state->cap) {
 		uint64_t fact = si->state->buf[si->index++];
-		if (fact == SIZE_MAX)
+		if (!fact)
 			continue;
 
 		*len  = 0;
-        	*pred = (uint16_t)fact;
+        	*pred = ((uint16_t)fact) - 1;
         	for (size_t i = 0; i < 3; i++) {
         	        const size_t arg = (uint16_t)(fact >> 16 * (i + 1));
         	        if (arg)
-        	                args[(*len)++] = arg;
+        	                args[(*len)++] = arg - 1;
         	        else
         	                break;
         	}
